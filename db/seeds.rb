f6a1da299f6fe7570ae54cf0284db5c9dca63b83
ROSS BICKMORE <rossbickmore@gmail.com>
2019-11-13 15:06:13 +0000

    added review functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-25.times do
+10.times do
     category = ["chinese", "italian", "japanese", "french", "belgian"].sample
     restaurant = Restaurant.new(
       name: Faker::Restaurant.name,
