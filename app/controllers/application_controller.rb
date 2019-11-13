eefa312bb6230abc08c9cc5f53ba67a5e02883fa
ROSS BICKMORE <rossbickmore@gmail.com>
2019-11-13 14:21:17 +0000

    created models, controllers, routes and styled with bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_restaurants
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurants
+      @restaurants = Restaurant.all
+    end
 end
